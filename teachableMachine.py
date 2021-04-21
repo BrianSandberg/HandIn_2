2565099d22b2fd5b9bc31aed868f64e51e617d2f
Oliver Werner <oow@ruc.dk>
2021-04-21 16:15:20 +0200

    Program can print prediction from list of labels

--- teachableMachine.py
+++ teachableMachine.py
@@ -1,6 +1,8 @@
+import math
 import tensorflow.keras
 from PIL import Image, ImageOps
 import numpy as np
+import json
 
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
@@ -14,7 +16,7 @@ model = tensorflow.keras.models.load_model('keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 # Replace this with the path to your image
-image = Image.open('test_marlboro.jpg')
+image = Image.open('test_hop.jpg')
 
 #resize the image to a 224x224 with the same strategy as in TM2:
 #resizing the image to be at least 224x224 and then cropping from the center
@@ -37,19 +39,24 @@ data[0] = normalized_image_array
 # run the inference
 prediction = model.predict(data)
 
-print(prediction)
-listTest = prediction.tolist()
-
-res_min = min (listTest, key=lambda x:int(x))
-res_max = max (listTest, key=lambda x:int(x))
-
-print(str(res_max))
+#print(prediction)
+listTest = (prediction*100).tolist()
 
 
-#sulul = test.split(' ')
-#print(sulul[0])
-#print(sulul[1])
-#print(sulul[2])
+#print(listTest[0])
+
+listToString = str(listTest[0])
+finalList = json.loads(listToString)
+
+#Prints the prediction and rounds this down to the nearst integer
+print("Prediction is ", math.floor(max(finalList)), "%")
+
+#Creates a list of every line in the desired document
+labels = open("labels.txt").readlines()
+
+#Prints the index in the labels-list corresponding to the highest value in the finalList
+print(labels[finalList.index(max(finalList))])
+
 
 
 
